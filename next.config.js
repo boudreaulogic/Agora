311890838ac215f6a5d4151a4c600317e7486c97
boudreaulogic <hunter@boudreaulogic.com>
2026-02-06 09:55:18 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+};
+
+module.exports = nextConfig;
